64e495b8056c3efd2b983776f5195603105b028c
hasmir <solishasmir@gmail.com>
2022-10-29 09:44:43 -0500

    Lab 5 formularios MVC sesiones cookies

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    require_once("controllers/principal_controller.php");
+    require_once("controllers/contacts_controller.php");
+    require_once("utils/seg.php");
+    if (count($_GET)==0)
+        call_user_func("principal_controller::index");
+    else{
+        $controlador = seg::decodificar($_GET["c"]);
+        $metodo = seg::decodificar($_GET["m"]);
+        call_user_func($controlador."_controller::".$metodo);
+    }
+    #principal_controller::index();
+    
+?>
